db76db147ca7afcc3167fbc745dff120467677a2
NoahMn <bss-com-09-17@cc.ac.mw>
2021-10-18 12:49:15 +0200

    commit

--- app/src/main/java/com/example/hotelmanagementapp/MainActivity.java
+++ app/src/main/java/com/example/hotelmanagementapp/MainActivity.java
@@ -2,13 +2,16 @@ package com.example.hotelmanagementapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
-
+ int splashTime = 1500;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(() -> startActivity(new Intent(MainActivity.this, Dashboard.class)), splashTime);
     }
 }
