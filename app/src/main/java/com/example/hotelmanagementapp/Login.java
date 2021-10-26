42477fbdc2049c1f643f1366a19d46452e7d32ec
Noah-Mn <bsc-com-09-17@cc.ac.mw>
2021-10-26 15:37:05 +0200

    commit

--- app/src/main/java/com/example/hotelmanagementapp/Login.java
+++ app/src/main/java/com/example/hotelmanagementapp/Login.java
@@ -2,13 +2,24 @@ package com.example.hotelmanagementapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+    Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        loginBtn = findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this,Dashboard.class));
+            }
+        });
     }
 }
