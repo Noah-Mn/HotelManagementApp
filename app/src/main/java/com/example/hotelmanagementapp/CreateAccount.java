1591a8821c31539617d9ad3f022d987efbb879e6
Noah-Mn <bsc-com-09-17@cc.ac.mw>
2021-10-27 13:59:31 +0200

    commit

--- app/src/main/java/com/example/hotelmanagementapp/CreateAccount.java
+++ app/src/main/java/com/example/hotelmanagementapp/CreateAccount.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class CreateAccount extends AppCompatActivity {
     TextView materialLogin;
+    Button signupBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +18,14 @@ public class CreateAccount extends AppCompatActivity {
         setContentView(R.layout.activity_create_account);
 
         materialLogin = findViewById(R.id.materialLogin);
+        signupBtn = findViewById(R.id.signupBtn);
+
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CreateAccount.this, Login.class));
+            }
+        });
         materialLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
