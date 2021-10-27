b4d2b2a1f55577d4864d75a610b7d82af7906f5f
Noah-Mn <bsc-com-09-17@cc.ac.mw>
2021-10-27 11:49:36 +0200

    commit

--- app/src/main/java/com/example/hotelmanagementapp/CreateAccount.java
+++ app/src/main/java/com/example/hotelmanagementapp/CreateAccount.java
@@ -2,13 +2,25 @@ package com.example.hotelmanagementapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class CreateAccount extends AppCompatActivity {
+    TextView materialLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        materialLogin = findViewById(R.id.materialLogin);
+        materialLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CreateAccount.this, Login.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/hotelmanagementapp/Login.java
+++ app/src/main/java/com/example/hotelmanagementapp/Login.java
@@ -6,15 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
     Button loginBtn;
+    TextView materialCreateAcc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         loginBtn = findViewById(R.id.loginBtn);
+        materialCreateAcc = findViewById(R.id.materialCreateAcc);
+
+        materialCreateAcc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this,CreateAccount.class));
+            }
+        });
+
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
