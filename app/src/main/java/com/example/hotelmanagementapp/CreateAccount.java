2648ff9d32ab1316be5e2b761cc4274470c9d52b
Noah <bsc-com-09-17@cc.ac.mw>
2021-11-02 17:44:07 +0200

    commit

--- app/src/main/java/com/example/hotelmanagementapp/CreateAccount.java
+++ app/src/main/java/com/example/hotelmanagementapp/CreateAccount.java
@@ -1,10 +1,12 @@
 package com.example.hotelmanagementapp;
 
 import android.app.DownloadManager;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -12,23 +14,41 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Objects;
 
 public class CreateAccount extends AppCompatActivity {
+    FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
+
+    private void updateUI(FirebaseUser currentUser) {
+
+    }
 
     TextView materialLogin;
+    TextInputLayout materialPassword, materialConfirmPassword, materialEmailAddress, materialUsername;
     MaterialButton signupBtn;
-    FirebaseDatabase firebaseDatabase;
-    DatabaseReference databaseReference;
-    DatabaseHelper databaseHelper;
+    ProgressDialog progressDialog;
     TextInputEditText textUsername, textEmailAddress, textPassword, textConfirmPassword;
+    String emailPattern = "^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]|[\\w-]{2,}))@"
+            + "((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+            + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\."
+            + "([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+            + "[0-9]{1,2}|25[0-5]|2[0-4][0-9]))|"
+            + "([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,42 +61,65 @@ public class CreateAccount extends AppCompatActivity {
         textEmailAddress = findViewById(R.id.textEmailAddress);
         textPassword = findViewById(R.id.textPassword);
         textConfirmPassword = findViewById(R.id.textConfirmPassword);
+        materialPassword = findViewById(R.id.materialPassword);
+        materialUsername = findViewById(R.id.materialUsername);
+        materialEmailAddress = findViewById(R.id.materialEmailAddress);
+        materialConfirmPassword = findViewById(R.id.materialConfirmPassword);
+        progressDialog = new ProgressDialog(this);
 
-        signupBtn.setOnClickListener(v -> {
-            firebaseDatabase = FirebaseDatabase.getInstance();
-            databaseReference = firebaseDatabase.getReference("Users");
+        signupBtn.setOnClickListener(v -> performAuthentication());
 
-            String Username = Objects.requireNonNull(textUsername.getText()).toString();
-            String EmailAddress = Objects.requireNonNull(textEmailAddress.getText()).toString();
-            String Password = Objects.requireNonNull(textPassword.getText()).toString();
-            String ConfirmPassword = Objects.requireNonNull(textConfirmPassword.getText()).toString();
+        materialLogin.setOnClickListener(v -> startActivity(new Intent(CreateAccount.this, Login.class)));
+    }
+    private void performAuthentication(){
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
 
-            databaseHelper = new DatabaseHelper(Username, EmailAddress, Password, ConfirmPassword);
-            if (
-                    Username.isEmpty() || EmailAddress.isEmpty() || Password.isEmpty() || ConfirmPassword.isEmpty()
-            ) {
-                Toast.makeText(getApplicationContext(),"Please fill all the fields",Toast.LENGTH_SHORT).show();
-            }else {
+        String Username = Objects.requireNonNull(textUsername.getText()).toString();
+        String EmailAddress = Objects.requireNonNull(textEmailAddress.getText()).toString();
+        String Password = Objects.requireNonNull(textPassword.getText()).toString();
+        String ConfirmPassword = Objects.requireNonNull(textConfirmPassword.getText()).toString();
+
+        if (!EmailAddress.matches(emailPattern)){
+            materialEmailAddress.setError("Please enter a valid email address");
+        }else if (Username.isEmpty()){
+            materialUsername.setError("Enter username");
+        } else if (EmailAddress.isEmpty()){
+            materialEmailAddress.setError("Enter email address");
+        }else if (Password.isEmpty()){
+            materialPassword.setError("Enter password");
+        }else if(ConfirmPassword.isEmpty()){
+            materialConfirmPassword.setError("Enter password");
+        }else {
                 if (Password.equals(ConfirmPassword)){
-                    databaseReference.child(Username).setValue(databaseHelper).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    progressDialog.setMessage("Registering please wait...");
+                    progressDialog.setTitle("Registration");
+                    progressDialog.setCanceledOnTouchOutside(false);
+                    progressDialog.show();
+                    firebaseAuth.createUserWithEmailAndPassword(EmailAddress,Password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                         @Override
-                        public void onSuccess(Void aVoid) {
-                            //User successfully added
-                            Toast.makeText(getApplicationContext(),"Account successfully created!",Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(CreateAccount.this, Login.class));
-                        }
-                    }).addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            // User not added!
-                            Toast.makeText(getApplicationContext(),"Failed to create account!",Toast.LENGTH_SHORT).show();
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                progressDialog.dismiss();
+                                Toast.makeText(getApplicationContext(),"Account successfully created!",Toast.LENGTH_SHORT).show();
+                                FirebaseUser user = firebaseAuth.getCurrentUser();
+                                updateUI(user);
+                                startActivity(new Intent(CreateAccount.this, Login.class));
+                            }else {
+                                progressDialog.dismiss();
+                                Toast.makeText(getApplicationContext(), ""+task.getException(), Toast.LENGTH_SHORT).show();
+                                updateUI(null);
+                            }
                         }
                     });
-                }else
-                    Toast.makeText(getApplicationContext(),"Passwords do not match",Toast.LENGTH_SHORT).show();
-            }
 
-        });
-        materialLogin.setOnClickListener(v -> startActivity(new Intent(CreateAccount.this, Login.class)));
+                }else{
+                    materialPassword.setError("Wrong password");
+                    materialConfirmPassword.setError("Wrong password");
+                    Toast.makeText(getApplicationContext(),"Passwords do not match",Toast.LENGTH_SHORT).show();
+                }
+
+
+        }
     }
 }
--- app/src/main/java/com/example/hotelmanagementapp/Login.java
+++ app/src/main/java/com/example/hotelmanagementapp/Login.java
@@ -1,16 +1,46 @@
 package com.example.hotelmanagementapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Objects;
 
 public class Login extends AppCompatActivity {
+    FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
+    TextInputEditText loginEmail, loginPassword;
+    TextInputLayout materialLogPassword, materialLogEmail;
     Button loginBtn;
     TextView materialCreateAcc, materialForgotPasswd;
+    ProgressDialog progressDialog;
+    String emailPattern = "^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]|[\\w-]{2,}))@"
+            + "((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+            + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\."
+            + "([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+            + "[0-9]{1,2}|25[0-5]|2[0-4][0-9]))|"
+            + "([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +50,11 @@ public class Login extends AppCompatActivity {
         loginBtn = findViewById(R.id.loginBtn);
         materialCreateAcc = findViewById(R.id.materialCreateAcc);
         materialForgotPasswd = findViewById(R.id.materialForgotPasswd);
+        loginEmail = findViewById(R.id.loginEmail);
+        loginPassword = findViewById(R.id.loginPassword);
+        progressDialog = new ProgressDialog(this);
+        materialLogEmail = findViewById(R.id.materialLogEmail);
+        materialLogPassword = findViewById(R.id.materialLogPassword);
 
         materialForgotPasswd.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,15 +65,54 @@ public class Login extends AppCompatActivity {
         materialCreateAcc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(Login.this,CreateAccount.class));
+                startActivity(new Intent(Login.this, CreateAccount.class));
             }
         });
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(Login.this,Dashboard.class));
+                performLogin();
             }
         });
+    }
+    private void performLogin(){
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+        String logEmailAddress = loginEmail.getText().toString();
+        String logPassword = loginPassword.getText().toString();
+
+        if (!logEmailAddress.matches(emailPattern)){
+            materialLogEmail.setError("Please enter a valid email address");
+        }else if (logEmailAddress.isEmpty()){
+            materialLogEmail.setError("Enter email address");
+        } else if (logPassword.isEmpty()){
+            materialLogPassword.setError("Please enter password");
+        }else{
+            progressDialog.setMessage("Logging in please wait...");
+            progressDialog.setTitle("Login");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            firebaseAuth.signInWithEmailAndPassword(logEmailAddress, logPassword).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        progressDialog.dismiss();
+                        Toast.makeText(getApplicationContext(), "Login successful", Toast.LENGTH_SHORT).show();
+                        FirebaseUser user = firebaseAuth.getCurrentUser();
+                        updateUI(user);
+                        startActivity(new Intent(Login.this, Dashboard.class));
+                    }else {
+                        progressDialog.dismiss();
+                        updateUI(null);
+                        Toast.makeText(Login.this, ""+task.getException(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    private void updateUI(FirebaseUser user) {
     }
 }
